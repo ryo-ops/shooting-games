bd7ef5348f5c675a721fdbdb8adb4928ae5c7b59
ryo-ops <ryo87643@gmail.com>
2025-07-10 00:54:53 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'simple-shooter-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/icon.svg'
+];
+
+// インストール時にキャッシュを作成
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// fetchイベントでリクエストに応答
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // キャッシュにあればそれを返す
+        if (response) {
+          return response;
+        }
+        // なければネットワークから取得
+        return fetch(event.request);
+      })
+  );
+});
